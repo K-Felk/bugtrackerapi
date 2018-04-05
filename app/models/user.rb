4b1d361a308a5934ae9356a9a2471a77f50fb703
kyle felker <felkerk@gvsu.edu>
2018-04-04 21:38:34 -0400

    allow thumbnail to be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     validates :lname,  presence: true
     validates :fname, presence: true
     validates :email, presence: true
+    validates :thumbnail, :allow_nil => true
 
     validates_format_of :email, :with => /\A[^@,\s]+@[^@,\s]+\.[^@,\s]+\z/
 
