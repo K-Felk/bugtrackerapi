0ab604a49e858301b40288a0cad019d0d554ac03
kyle felker <felkerk@gvsu.edu>
2018-04-04 21:44:04 -0400

    thumbnail be nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ApplicationRecord
     validates :lname,  presence: true
     validates :fname, presence: true
     validates :email, presence: true
-    validates :thumbnail, :allow_nil => true
+    validates :thumbnail, format: {with: /[^\s](.gif|.jpg|.png)\z/}, :allow_nil => true
 
     validates_format_of :email, :with => /\A[^@,\s]+@[^@,\s]+\.[^@,\s]+\z/
 
-    validates_format_of :thumbnail, :with => /[^\s](.gif|.jpg|.png)\z/
 
     validates_uniqueness_of :email
 
